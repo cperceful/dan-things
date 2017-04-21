addd117b431a2fb11d9eea1a20cd85f9c99228e6
cperceful <cpercef1@gmail.com>
2017-04-21 15:07:27 -0500

    Initial commit

---
+++ src/main/java/com/cperceful/DanThingsApplication.java
@@ -0,0 +1,12 @@
+package com.cperceful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanThingsApplication.class, args);
+	}
+}
